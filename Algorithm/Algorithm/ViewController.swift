ddc444dae23d3be23739d171d3304488a347e4ad
Felix Xie <543547858@qq.com>
2019-11-02 19:12:05 +0800

    add：add two numbers

--- Algorithm/Algorithm/ViewController.swift
+++ Algorithm/Algorithm/ViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
-
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+class Solution {
+    /// 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+    /// 输出：7 -> 0 -> 8
+    /// 原因：342 + 465 = 807
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        var carry = 0,l1 = l1,l2 = l2
+        let head = ListNode(0)
+        var node = head
+        while l1 != nil || l2 != nil || carry != 0 {
+            if l1 != nil {
+                carry += l1!.val
+                l1 = l1!.next
+            }
+            if l2 != nil {
+                carry += l2!.val
+                l2 = l2!.next
+            }
+            node.next = ListNode(carry%10)
+            node = node.next!
+            carry = carry / 10
+        }
+        return head.next
+    }
+}
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
 //        var a = [1,6,3,9,4]
 //        print("\(bubble(&a))")
 //        print("\(quick(&a))")
