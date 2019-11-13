9c0e07d571cfacafc35274a15436f7f99d8b28f1
Felix Xie <543547858@qq.com>
2019-11-13 23:04:48 +0800

    add：最长回文子串动态规划

--- Algorithm/Algorithm/LongestPalindrome.swift
+++ Algorithm/Algorithm/LongestPalindrome.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class LongestPalindrome: NSObject {
+    /// 中心扩散
     func longestPalindrome(_ s: String) -> String {
         if s.count == 0 { return "" }
         var start = 0, end = 0, maxLen = 0
@@ -37,4 +38,27 @@ class LongestPalindrome: NSObject {
         return r - l - 1
     }
     
+    /// 动态规划
+    func longestPalindrome2(_ s: String) -> String {
+        if s.count < 1 { return "" }
+        let len = s.count
+        var dp = [[Bool]](repeating: [Bool](repeating: false, count: len), count: len)
+        var longestPalindrome = 1
+        var longestPalindromeStr = String(s.prefix(1))
+        let chars: [Character] = Array(s)
+        for r in 1 ..< len {
+            for l in 0 ..< r {
+                if chars[l]==chars[r] && (r-l<=2 || dp[l+1][r-1]) {
+                    dp[l][r] = true
+                    if r-l+1 > longestPalindrome {
+                        longestPalindrome = r-l+1
+                        let startIndex = s.index(s.startIndex, offsetBy: l)
+                        let endIndex = s.index(s.startIndex, offsetBy: r)
+                        longestPalindromeStr = String(s[startIndex...endIndex])
+                    }
+                }
+            }
+        }
+        return longestPalindromeStr
+    }
 }
