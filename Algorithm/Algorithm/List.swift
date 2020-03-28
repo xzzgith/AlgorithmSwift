1ba7261a41b35b321b2997a3f2b0b7e2fc82c405
Felix Xie <543547858@qq.com>
2020-03-28 10:53:27 +0800

    add：best-time-to-buy-and-sell-stock

--- Algorithm/Algorithm/List.swift
+++ Algorithm/Algorithm/List.swift
@@ -134,4 +134,16 @@ class List: NSObject {
         if needAdd { ans.insert(1, at: 0) }
         return ans
     }
+    // MARK: - best-time-to-buy-and-sell-stock
+    /// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
+    func maxProfit(_ prices: [Int]) -> Int {
+        if prices.count <= 1 { return 0 }
+        var minPrice = prices[0]
+        var maxProfit = 0
+        for i in 1 ..< prices.count {
+            maxProfit = max(maxProfit, prices[i] - minPrice)
+            minPrice = min(minPrice, prices[i])
+        }
+        return maxProfit
+    }
 }
